197c7e26bc9c681ff22915e75dfca41152caa4c2
ZeeshanSiddiqui880 <zeeshansiddiqui880@gmial.com>
2025-08-02 20:50:41 +0530

    react-routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-   
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './landing_page/home/HomePage.jsx'
- 
+import SignUp from './landing_page/signup/SignUp.jsx'
+import AboutPage from './landing_page/about/AboutPage.jsx'
+import ProductPage from './landing_page/products/ProductPage.jsx'
+import PricingPage from './landing_page/pricing/PricingPage.jsx'
+import SupportPage from './landing_page/support/SupportPage.jsx'
+import PageNotFound from './landing_page/PageNotFound.jsx'
+import Navbar from './landing_page/Navbar.jsx'
+import Footer from './landing_page/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HomePage />
-  </StrictMode>,
+
+  <BrowserRouter> 
+    <Navbar/>
+    <Routes>
+      <Route path='/'  element={<HomePage/>}/>
+      <Route path='/signup'  element={<SignUp/>}/>
+      <Route path='/about'  element={<AboutPage/>}/>
+      <Route path='/product'  element={<ProductPage/>}/>
+      <Route path='/pricing'  element={<PricingPage/>}/>
+      <Route path='/support'  element={<SupportPage/>}/>
+      <Route path='*'  element={<PageNotFound/>}/>
+    </Routes>
+    <Footer/>
+  </BrowserRouter>
 )
